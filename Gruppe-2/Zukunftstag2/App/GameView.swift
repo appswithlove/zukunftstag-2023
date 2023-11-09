5fc38e45055d37873ec86743a2cfd32901561d2e
Alain Stulz <alain@appswithlove.com>
2023-11-09 15:33:54 +0100

    Add more characters and animations to game

--- Gruppe-2/Zukunftstag2/App/GameView.swift
+++ Gruppe-2/Zukunftstag2/App/GameView.swift
@@ -12,12 +12,27 @@ struct GameView: View {
     @State private var playerOffset = CGSize.zero
     @State private var dragOffset = CGSize.zero
     
-    @State var monsterOffsetX: CGFloat = 0.0
-    @State var monsterOffsetY: CGFloat = 200.0
+    @State var monsterOffsetX0: CGFloat = 000.0
+    @State var monsterOffsetY0: CGFloat = 200.0
+    
+    @State var monsterOffsetX1: CGFloat = 100.0
+    @State var monsterOffsetY1: CGFloat = 200.0
+    
+    @State var monsterOffsetX2: CGFloat = -100.0
+    @State var monsterOffsetY2: CGFloat = 200.0
     
     let screenBounds = UIScreen.main.bounds
     
+    @State var is0MovingRight = true
+    @State var is1MovingRight = true
+    @State var is2MovingRight = false
+    
+    @State var playerCharacter = "👻"
+    @State var playerCharacterScale: CGFloat = 1.0
+    
     let onFinish: () -> Void
+    
+    let timer = Timer.publish(every: 0.005, on: .main, in: .common, options: nil).autoconnect()
     
     var body: some View {
         ZStack {
@@ -25,31 +40,74 @@ struct GameView: View {
                 Spacer()
                 
                 Text("🏁 ZIEL 🏁")
-                    .font(.headline)
+                    .font(.title)
                     .bold()
             }
             
-            Text("👾")
-                .font(.system(size: 64))
-                .offset(x: monsterOffsetX, y: monsterOffsetY)
+            VStack(spacing: 0) {
+                Text("👾")
+                    .font(.system(size: 64))
+                    .offset(x: monsterOffsetX0, y: monsterOffsetY0)
+                Text("👾")
+                    .font(.system(size: 64))
+                    .offset(x: monsterOffsetX1, y: monsterOffsetY1)
+                Text("👾")
+                    .font(.system(size: 64))
+                    .offset(x: monsterOffsetX2, y: monsterOffsetY2)
+            }
             
-            Text("👻")
+            Text(playerCharacter)
                 .font(.system(size: 64))
+                .scaleEffect(playerCharacterScale)
                 .offset(x: playerOffset.width + dragOffset.width,
                         y: playerOffset.height + dragOffset.height)
                 .gesture(DragGesture()
                     .onChanged { value in
                         dragOffset = value.translation
                         checkIsWithinBounds()
+                        checkIsOnGoal()
                     }
                     .onEnded({ value in
                         dragOffset = .zero
                         playerOffset.width += value.translation.width
                         playerOffset.height += value.translation.height
                         checkIsWithinBounds()
+                        checkIsOnGoal()
                     })
                 )
-        }
+        }.onReceive(timer, perform: { _ in
+            if abs(monsterOffsetX0) > (screenBounds.width / 2) - 50 {
+                is0MovingRight.toggle()
+            }
+            
+            if abs(monsterOffsetX1) > (screenBounds.width / 2) - 50 {
+                is1MovingRight.toggle()
+            }
+            
+            if abs(monsterOffsetX2) > (screenBounds.width / 2) - 50 {
+                is2MovingRight.toggle()
+            }
+            
+            let speed = 1.0
+            
+            if is0MovingRight {
+                monsterOffsetX0 += speed
+            } else {
+                monsterOffsetX0 -= speed
+            }
+            
+            if is1MovingRight {
+                monsterOffsetX1 += speed
+            } else {
+                monsterOffsetX1 -= speed
+            }
+            
+            if is2MovingRight {
+                monsterOffsetX2 += speed
+            } else {
+                monsterOffsetX2 -= speed
+            }
+        })
     }
     
     func checkIsWithinBounds() {
@@ -57,10 +115,27 @@ struct GameView: View {
         let totalOffsetY = playerOffset.height + dragOffset.height
         
         if abs(totalOffsetX) > (screenBounds.width / 2) - 5 {
-            onFinish()
+            finishGame()
         }
         
         if abs(totalOffsetY) > (screenBounds.height / 2) - 5 {
+            finishGame()
+        }
+    }
+    
+    func checkIsOnGoal() {
+        let totalOffsetY = playerOffset.height + dragOffset.height
+
+        if totalOffsetY > (screenBounds.height / 2 - 70) {
+            finishGame()
+        }
+    }
+    
+    func finishGame() {
+        playerCharacter = "💥"
+        playerCharacterScale = 2.0
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             onFinish()
         }
     }
